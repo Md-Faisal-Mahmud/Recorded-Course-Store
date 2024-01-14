3edb1d045e6ac9e25b879e3f0d6aac0b9810c5ae
NakibBracu <nakibrahamanbracu18@gmail.com>
2024-01-14 12:58:31 +0600

    Log4Net configured and successfully writing log

--- src/RCS.Web/Program.cs
+++ src/RCS.Web/Program.cs
@@ -1,27 +1,45 @@
+using log4net;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+//Log4net 
+builder.Logging.ClearProviders();
+builder.Logging.AddLog4Net();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+
+var log = LogManager.GetLogger(typeof(Program));
+
+try {
+    var app = builder.Build();
+
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    log.Info("Application is starting");
+    app.Run();
+
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+catch (Exception ex)
+{
+    log.Fatal($"API can not start.\n{ex}");
+}
