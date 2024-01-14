8077b3e4923eba258733ce12f9cdd77a71a755bd
NakibBracu <nakibrahamanbracu18@gmail.com>
2024-01-14 11:28:12 +0600

    Blank Solution and UI Layer Created!

---
+++ src/RCS.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RCS.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
