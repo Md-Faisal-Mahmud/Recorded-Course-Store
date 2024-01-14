bd4182070d0fcec75266e13cf9e9434090636fe9
NakibBracu <nakibrahamanbracu18@gmail.com>
2024-01-14 21:00:27 +0600

    Course added service method implemented!

---
+++ src/RCS.Services/Services/CourseService.cs
@@ -0,0 +1,37 @@
+﻿using RCS.Data.Entities;
+using RCS.Data.Enums;
+using RCS.Data.UnitOfWorks;
+using System.Xml.Linq;
+
+namespace RCS.Services.Services
+{
+    public class CourseService : ICourseService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+
+        public CourseService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Course> AddCourseAsync(string title, string description, string videoUrl, string thumbnailImage, decimal price, DifficultyLevel difficultyLevel)
+        {
+            Course course = new Course()
+            {
+                Title = title,
+                Description = description,
+                VideoURL = videoUrl,
+                ThumbnailImage = thumbnailImage,
+                Price = price,
+                DifficultyLevel = difficultyLevel
+            };
+
+            await _unitOfWork.BeginTransaction();
+            await _unitOfWork.Courses.AddAsync(course);
+            await _unitOfWork.Commit();
+
+            return course;
+        }
+    }
+}
---
+++ src/RCS.Services/Services/ICourseService.cs
@@ -0,0 +1,10 @@
+﻿using RCS.Data.Entities;
+using RCS.Data.Enums;
+
+namespace RCS.Services.Services
+{
+    public interface ICourseService
+    {
+        Task<Course> AddCourseAsync(string title, string description, string videoUrl, string thumbnailImage, decimal price, DifficultyLevel difficultyLevel);
+    }
+}
