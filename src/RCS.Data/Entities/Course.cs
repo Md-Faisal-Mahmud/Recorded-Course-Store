19c5b7acc910dcb4f78c56a706a25a4e8fde7be0
NakibBracu <nakibrahamanbracu18@gmail.com>
2024-01-14 17:30:29 +0600

    set connection string and also add data layer and some classes and also a enum!

---
+++ src/RCS.Data/Entities/Course.cs
@@ -0,0 +1,20 @@
+﻿using RCS.Data.Enums;
+
+namespace RCS.Data.Entities
+{
+    public class Course : IEntity<Guid>
+    {
+        public Guid Id { get; set; }
+        public virtual string Title { get; set; }
+
+        public virtual string? Description { get; set; }
+
+        public virtual string VideoURL { get; set; }
+
+        public virtual string? ThumbnailImage { get; set; }
+        public virtual decimal Price { get; set; }
+
+        public virtual DifficultyLevel DifficultyLevel { get; set; }
+
+    }
+}
